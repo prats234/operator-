d121d910ed4162c915ed584850b563684622b107
prats234 <109065276+prats234@users.noreply.github.com>
2022-07-11 21:53:02 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,34 @@
+public class operators
+{
+    public static void main(String[] args)
+    {
+        int num1 = 10;
+        int num2 = 20;
+        double num3 = 35.5;
+        short num4 = 46;
+        int num5 = 50;
+        {
+            if (num1 > num2)
+            {
+                System.out.println("num1 is greater");
+            }
+            else if (num2 > num3 )
+            {
+                System.out.println("num2 is greater");
+            }
+            else if (num3 > num4 )
+            {
+                System.out.println("num3 is greater");
+            }
+            else if (num4 > num5 )
+            {
+                System.out.println("num4 is greater");
+            }
+            else
+            {
+                System.out.println("num5 is greater");
+            }
+        }
+    }
+}
+
